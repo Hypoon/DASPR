201975b662870ac808ce018b1dd60241ac337a07
John Bonini <bonini57@gmail.com>
2012-01-24 21:16:16 -0500

    Datasheets

--- datasheets/paymentoptions.aspx_files/151948540Top.js
+++
@@ -1,9 +0,0 @@
-function OAS_RICH(position) {
-if (position == 'Top') {
-document.write ('		<a href="https://oasc05.247realmedia.com/RealMedia/ads/click_lx.ads/my.verizon.com/billview/printpage/L33/988056384/Top/VDSL/House_728x90_RoomtoLearn_2011-03/House_728x90_RoomtoLearn_CableClutter_2011-03.html/5950684f5a55356747687741422f5670" target="_top">\n');
-document.write ('    <img src="https://oasc17.247realmedia.com/RealMedia/ads/Creatives/VDSL/House_728x90_RoomtoLearn_2011-03/RTL_cableclutter_728x90_v5_31711.jpg/1301330806" style="border:0px black solid;" width="728" height="90" alt="" />\n');
-document.write ('</a>\n');
-document.write ('\n');
-document.write ('\n');
-}
-}
--- datasheets/paymentoptions.aspx_files/getwb31.js
+++
@@ -1,8 +0,0 @@
-
-rewriteHTML('scbwbnotice','');
-rewriteHTML('scbwblinktext','Glassboro,&nbsp;NJ&nbsp;08028');
-rewriteHTML('scbwbicon','<a href=\'http://weather.weatherbug.com/NJ/Glassboro-weather.html?zcode=z6726&zip=08028\' target=\'_blank\'><img src="https://www22.verizon.com/images/headerfooter/wb/08partlyCloudyNight.jpg" width="33" height="33" border="0" alt="Partly Cloudy" title="Partly Cloudy" /></a>');
-rewriteHTML('scbwbtemp','73&deg;F');
-rewriteHTML('scbwbhilo','<br />Lo&nbsp;61&deg;F');
-rewriteHTML('scbwbpb','Powered&nbsp;by&nbsp;<a href=\'http://weather.weatherbug.com/NJ/Glassboro-weather.html?zcode=z6726&zip=08028\' target=\'_blank\' style=\'color:#06c;\'>WeatherBug</a>');
-		
