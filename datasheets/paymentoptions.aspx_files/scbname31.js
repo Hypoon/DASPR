e559c8d9c3cc64a40ba5fe8327967b6cbff64772
John Bonini <bonini57@gmail.com>
2012-01-24 21:16:16 -0500

    Datasheets

--- datasheets/paymentoptions.aspx_files/scbname31.js
+++
@@ -1,8 +0,0 @@
-
-<!-- mp_trans_disable_start -->
-var strFN='JOHN';
-<!-- mp_trans_disable_end -->
-var strWelcome='Welcome,';
-if(strFN!=''){strWelcome='Welcome&nbsp;'+strFN+',';}
-rewriteHTML('scbusername',strWelcome);
-swLayer('scbuser1','show','scbuser2','show','scbuser3','show');
